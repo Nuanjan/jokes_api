eb965c6815be10165b49cbbca1d25327c72f4e31
Nuanjan <nuanjan.schluntz@gmail.com>
2021-11-01 15:34:46 -0400

    create Jokes API using express mongoose mongoDB

---
+++ server/models/joke.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const JokeSchema = new mongoose.Schema(
+  {
+    setup: {
+      type: String,
+      required: [true, "Setup is required"],
+      minLength: [10, "Setup must be at least 10 characters long"],
+    },
+    punchline: {
+      type: String,
+      required: [true, "Punchline is required"],
+      minLength: [10, "Punchline must be at least 13 characters long"],
+    },
+  },
+  { timestamps: true }
+);
+
+const Joke = mongoose.model("Joke", JokeSchema);
+
+module.exports = Joke;
